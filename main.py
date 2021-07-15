dd272c1b8cf99c8b5f4b6974339a86446f62ed50
Регина Салаватова <regina_salavatova@mail.ru>
2021-07-15 21:59:17 +0500

    third

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template,  redirect
 from data import db_session
 from data.users import User
 from data.registerForm import RegisterForm
@@ -58,10 +58,10 @@ def reqistr():
     form = RegisterForm()
     if form.validate_on_submit():
         if form.password.data != form.password_again.data:
-            return render_template('registr.html', title='Регистрация',form=form, message="Пароли не совпадают")
+            return render_template('registr.html', title='Регистрация', form=form, message="Пароли не совпадают")
         session = db_session.create_session()
         if session.query(User).filter(User.email == form.email.data).first():
-            return render_template('registr.html', title='Регистрация', form=form, message="Такой пользователь уже есть")
+            return render_template('registr.html', title='Регистрация', form=form, message="Этот пользователь уже есть")
         user = User(email=form.email.data, name=form.name.data)
         user.set_password(form.password.data)
         session.add(user)
@@ -70,8 +70,6 @@ def reqistr():
     return render_template('registr.html', title='Регистрация', form=form)
 
 
-
 if __name__ == "__main__":
     db_session.global_init("db/blogs.sqlite")
     app.run()
-
